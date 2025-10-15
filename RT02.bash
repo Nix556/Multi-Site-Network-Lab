ab7c060813637a1c6de805610acaf94ca35e7497
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-10-15 11:35:32 +0200

    Add files via upload

---
+++ RT02.bash
@@ -0,0 +1,61 @@
+! ==========================
+! Router RT02 - Nyborg
+! ==========================
+
+conf t
+
+! --- Hostname og domæne ---
+hostname RT02
+ip domain-name nyborg.local
+
+! --- Brugere til SSH ---
+username admin privilege 15 secret cisco
+crypto key generate rsa modulus 2048
+ip ssh version 2
+
+! --- LAN SUBINTERFACES ---
+interface GigabitEthernet0/0.10
+ description VLAN 10 - Klient Nyborg
+ encapsulation dot1Q 10
+ ip address 10.20.10.1 255.255.255.0
+ ip ospf 1 area 0
+ exit
+
+interface GigabitEthernet0/0.99
+ description VLAN 99 - Management Nyborg
+ encapsulation dot1Q 99
+ ip address 10.20.99.1 255.255.255.0
+ ip ospf 1 area 0
+ exit
+
+! --- WAN LINK Odense↔Nyborg ---
+interface Serial0/0/0
+ description Nyborg - Odense
+ ip address 172.16.1.2 255.255.255.252
+ ip ospf 1 area 0
+ exit
+
+! --- Default route til Odense (for trafik mod internet eller servere) ---
+ip route 0.0.0.0 0.0.0.0 172.16.1.1
+
+! --- OSPF ---
+router ospf 1
+ router-id 2.2.2.2
+ network 10.20.10.0 0.0.0.255 area 0
+ network 10.20.99.0 0.0.0.255 area 0
+ network 172.16.1.0 0.0.0.3 area 0
+ passive-interface GigabitEthernet0/0.10
+ passive-interface GigabitEthernet0/0.99
+ exit
+
+! --- VTY / SSH adgang ---
+line vty 0 4
+ transport input ssh
+ login local
+ exec-timeout 10
+ logging synchronous
+ exit
+
+service password-encryption
+end
+write memory
---
+++ SW02.bash
@@ -0,0 +1,40 @@
+! ==========================
+! Switch SW02 - Nyborg
+! ==========================
+
+conf t
+
+hostname SW02
+ip domain-name nyborh.local
+
+! --- VLANs ---
+vlan 10
+ name Klient
+vlan 99
+ name Management
+
+! --- Management interface VLAN 99 ---
+interface vlan 99
+ ip address 10.20.99.2 255.255.255.0
+ no shutdown
+
+ip default-gateway 10.20.99.1
+
+! --- SSH brugere ---
+username admin privilege 15 secret cisco
+crypto key generate rsa modulus 2048
+ip ssh version 2
+
+! --- VTY linjer ---
+line vty 0 4
+ transport input ssh
+ login local
+ exec-timeout 10
+ logging synchronous
+ exit
+
+service password-encryption
+no ip http server
+no ip http secure-server
+end
+write memory
