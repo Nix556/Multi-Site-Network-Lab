876530734c07c8d7336f418a59f21863299e2b11
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-10-15 12:26:21 +0200

    Update SW01.bash

--- SW01.bash
+++ SW01.bash
@@ -16,6 +16,41 @@ vlan 30
  name Printer
 vlan 99
  name Management
+ exit
+
+! --- Trunk-port til router RT01 ---
+interface GigabitEthernet0/1
+ switchport mode trunk
+ switchport trunk allowed vlan 10,20,30,99
+ exit
+
+! --- Access-porte for klienter VLAN 10 ---
+interface range GigabitEthernet0/2 - 10
+ switchport mode access
+ switchport access vlan 10
+ spanning-tree portfast
+ exit
+
+! --- Access-porte for servere VLAN 20 ---
+interface range GigabitEthernet0/11 - 12
+ switchport mode access
+ switchport access vlan 20
+ spanning-tree portfast
+ exit
+
+! --- Access-porte for printere VLAN 30 ---
+interface range GigabitEthernet0/13 - 14
+ switchport mode access
+ switchport access vlan 30
+ spanning-tree portfast
+ exit
+
+ ! --- Access-porte for management VLAN 99 ---
+interface range GigabitEthernet0/15 - 16
+ switchport mode access
+ switchport access vlan 99
+ spanning-tree portfast
+ exit
 
 ! --- Management interface VLAN 99 ---
 interface vlan 99
@@ -41,4 +76,4 @@ service password-encryption
 no ip http server
 no ip http secure-server
 end
-write memoryL
+write memory
