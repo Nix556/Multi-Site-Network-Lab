b2d26fbf73f8ec7b1969b2a1f4b2d6e10a2dbc35
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-10-30 12:11:35 +0100

    Update server rename script to English

---
+++ powershell/DC01/01-Rename-And-Restart.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Renames the server to DC01 and restarts.
+#>
+
+$NewName = "DC01"
+
+Write-Host "Changing server name to $NewName ..." -ForegroundColor Cyan
+Rename-Computer -NewName $NewName -Force
+
+Write-Host "Restarting to apply new server name..." -ForegroundColor Yellow
+Restart-Computer
